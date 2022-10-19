eaf4f8d5d39fdc623b4e9de46afa4dbdf6b609f3
pablozagnoli <pablozagnoli@outlook.com>
2022-10-18 23:13:40 -0300

    docs atualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { Routes, RouterModule } from '@angular/router'
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'listarPensamento',
+    redirectTo: 'index.html',
     pathMatch: 'full'
   },
   {
